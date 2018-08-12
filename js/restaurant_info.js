29a750a4952447e5c78d86318bdac83aacd96f61
Ina Chemerko <innachemerko@gmail.com>
2018-08-11 22:13:05 -0700

    Add alt attribute, change quotes

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -89,7 +89,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = `An image from the restaurant ${restaurant.name}`;
+  image.alt = `Picture of Restaurant ${restaurant.name}`;
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
